e98d0280785737113136440ad28d4cc20e930005
scric <944242310@qq.com>
2017-03-18 23:18:57 +0800

    输入输出

---
+++ LearnForJava/第二章-Java的基本程序设计结构/InAndOut_Put.java
@@ -0,0 +1,12 @@
+/**
+ * Created by scric on 2017/3/18.
+ * @version 1.0
+ * @author scric
+ */
+public class InAndOut_Put {
+    public static void main(String[] args) {
+        // 输入与输出。
+
+        //为了使程序能够接收输入
+    }
+}
