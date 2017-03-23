eba28473d2a453a28883ccb714203547b8e5fbe6
scric <944242310@qq.com>
2017-03-23 22:40:04 +0800

    国足胜利

---
+++ LearnForJava/中国大学mooc作业/China.java
@@ -0,0 +1,11 @@
+/**
+ * Created by scric on 2017/3/23.
+ *
+ */
+public class China {
+    public static void main(String[] args) {
+        for (; ; ) {
+            System.out.println("国足胜利");
+        }
+    }
+}
